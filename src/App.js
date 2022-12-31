f5779e031d2e96817e2bd98a64c7715ed3eb2bee
Naasir Osman <109971732+naasirosman@users.noreply.github.com>
2022-12-31 11:44:02 +0000

    html/css/setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+      <div className='calculator'>
+      <div className="calculator-display">0</div>
+      <div className="calculator-keypad">
+        <button className="calculator-key">AC</button>
+        <button className="calculator-key">+/-</button>
+        <button className="calculator-key">%</button>
+        <button className="calculator-key">÷</button>
+        <button className="calculator-key">7</button>
+        <button className="calculator-key">8</button>
+        <button className="calculator-key">9</button>
+        <button className="calculator-key">x</button>
+        <button className="calculator-key">4</button>
+        <button className="calculator-key">5</button>
+        <button className="calculator-key">6</button>
+        <button className="calculator-key">-</button>
+        <button className="calculator-key">1</button>
+        <button className="calculator-key">2</button>
+        <button className="calculator-key">3</button>
+        <button className="calculator-key">+</button>
+        <button className="calculator-key">0</button>
+        <button className="calculator-key">.</button>
+        <button className="calculator-key">=</button>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
